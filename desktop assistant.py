5d4c7c678fd2dc8a57397d3c8e13558a4cdd9535
Rajdeep <145921893+Rajdeep7424@users.noreply.github.com>
2024-10-11 20:30:06 +0530

    no mic needed and can also use without internet if

--- desktop assistant.py
+++ desktop assistant.py
@@ -1,6 +1,6 @@
 import pyttsx3
 import datetime
-import speech_recognition as sr
+#import speech_recognition as sr 
 import wikipedia
 import webbrowser
 import os
@@ -8,6 +8,7 @@ import os
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 155)
 
 def speak(audio):
     engine.say(audio)
@@ -16,59 +17,103 @@ def speak(audio):
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
-        speak("Good morning Sir")
-    elif hour>=12 and hour<4:
-        speak("Good afternoon Sir")
+        speak("Good morning sir")
+        print("Computer: Good morning sir")
+    elif hour>=12 and hour<16:
+        speak("Good afternoon sir")
+        print("Computer: Good afternoon sir")
     else:
-        speak("Good Evening Sir")
+        speak("Good Evening sir")
+        print("Computer: Good Evening sir")
 
-    speak("I am your Virtual Assistant JARVIS.")
-    
-def takeCommand():
-    r =sr.Recognizer()
-    with sr.Microphone() as source:
-        print("listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-    try:
-        print("recognizing...")
-        query = r.recognize_google(audio, language ='en-in')
-        print("user said : ", query)
-    except Exception as e:
-        print(e)
-        speak("Sorry Sir, can you repeat that again?")
-        return "None"
-    return query
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")  
+        speak(f"The time is {strTime}")
+        print(f"The time is: {strTime}")
+    speak("I am your Virtual Assistant named iswia.")
+    print("Computer: I am your Virtual Assistant named Iswia.")
+
+while True:
+
+    query = input("User: ")
+    next = str(query("> "))
+   
+    if "your name" in query:
+            speak("my name is Iswia.")
+            print("Computer: My name is Iswia.")
+
+    elif "google for" in query:
+            search_term = query.split("for")[-1]
+            url = f"https://www.google.com/results?search_query={search_term}"
+            webbrowser.get().open(url)
+            speak(f'Here is what I found for {search_term} on google')
+            print("Computer: Here is what I found for ")
+            print(search_term)
+
+    elif "`" in query:
+            search_term = query.split("`") [-1]
+            speak(f'{search_term}')
+            print(f'Computer: {search_term}')
+
+    elif "youtube for" in query:
+            search_term = query.split("for")[-1]
+            url = f"https://www.youtube.com/results?search_query={search_term}"
+            webbrowser.get().open(url)
+            speak(f'Here is what I found for {search_term} on youtube')
+            print("Computer: Here is what I found for")
+            print(search_term)
+
+    elif 'open gmail' in query:
+            url = f"https://www.gmail.com/"
+            webbrowser.get().open(url)
+            speak("Opening gmail")
+            print("Computer: Opening gmail")
+
+    elif 'my web' in query:
+            url = f"https://technicalfacty.data.blog/"
+            webbrowser.get().open(url)
+            speak("Opening your website sir.")
+            print("Computer: Opening your website sir.")
+
+    elif 'open code' in query:
+            open('path')
+            path = "C:\Program Files\Microsoft VS Code\Code.exe"
+            speak("Opening Visual Studio Code")
+            print("Computer: Opening Visual Studio Code")
+            os.startfile(path)
+
+    elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"the time is {strTime}")
+            print(f"Computer: The time is: {strTime}")
+
+    elif "offline" in query:
+            speak("Turning off all my systems")
+            print("Computer: Turning off all my systems")
+            hour = int(datetime.datetime.now().hour)
+            if hour>=0 and hour<12:
+             speak("Bye! sir")
+             print("Computer: Bye! sir")
+            elif hour>=12 and hour<21:
+             speak("See you soon sir")
+             print("Computer: See you soon! sir")
+            else:
+             speak("Good night sir")
+             print("Computer: Good night sir")
+            exit()
+
+    elif "-" in query:
+            search_term = query.split("-")[-1]
+            url = f"https://www.google.com/results?search_query={search_term}"
+            webbrowser.get().open(url)
+            speak(f'Here is what I found for {search_term} on google')
+            print("Computer: Here is what I found for")
+            print(search_term)
+
+
 
 if __name__ == "__main__":
     wishMe()
     while True:
-        speak("Please tell me how can i help you?")
         query = takeCommand().lower()
-        if 'wikipedia' in query:
-                speak("searching in wikipedia")
-                query = query.replace("wikipedia", "")
-                results = wikipedia.summary(query, sentences = 2)
-                speak("According to wikipedia")
-                print(results)
-                speak(results)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-            speak("Opening Youtube Sir")
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-            speak("Opening Google Sir")
-        elif 'open gmail' in query:
-            webbrowser.open("gmail.com")
-            speak("Opening Gmail Sir")
-        #elif 'open code' in query:
-         #   open('path')
-          #  path = "C:\\Program Files\\Microsoft VS Code\\Code.exe"
-           # speak("Opening Visual Studio Code Sir")
-        elif 'time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"the time is {strTime}")
-        elif 'go offline' in query:
-            speak("turning off my systems sir, see you soon!")
-            exit()
+# python -u "f:\I.S.W.I.A\I.S.W.I.A.py"
